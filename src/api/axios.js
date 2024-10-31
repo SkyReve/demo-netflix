a41136d28990ab1e7cc34277df5756753bd04fef
YUN PARK <hanlsin@gmail.com>
2024-10-31 01:50:45 -0400

    Add env for project ID

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+console.debug(" >>>>>>>>> ", process.env.REACT_APP_PROJECT_ID);
+
 const instance = axios.create({
-  baseURL : "http://api.hellreve.world/project/og4j9y2pkw5n63p7/",
-  params : {
-    api_key : "c3f199fe9a0d3b8b7ccb5b5700521028",
-    language : "ko-KR"
-  }    
-})
+  baseURL: `http://api.hellreve.world/project/${process.env.REACT_APP_PROJECT_ID}/`,
+  params: {
+    api_key: "c3f199fe9a0d3b8b7ccb5b5700521028",
+    language: "ko-KR",
+  },
+});
 
 export default instance;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,59 +1,64 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { CommonStateContext } from '../App';
-import './Nav.css';
-
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CommonStateContext } from "../App";
+import "./Nav.css";
 
 function Nav() {
   const [show, setShow] = useState(false);
   const [searchValue, setSearchValue] = useState("");
   const { setIsClicked } = useContext(CommonStateContext);
   const navigate = useNavigate();
-  const PUBLIC_URL = 'https://github.com/SkyReve/demo-netflix/blob/main/public'
-  
-  useEffect(()=>{
+  const PUBLIC_URL = "";
+
+  useEffect(() => {
     window.addEventListener("scroll", () => {
-        if(window.scrollY > 50) {
-          setShow(true);    
-        } else{
-          setShow(false);
-        }
-    })
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
 
     return () => {
       window.removeEventListener("scroll", () => {});
-    }
+    };
   }, []);
 
   const handleChange = (e) => {
     setSearchValue(e.target.value);
     navigate(`/search?q=${e.target.value}`);
-  }
+  };
 
   const goIndex = () => {
     setSearchValue("");
     setIsClicked(false);
     navigate("/");
-  }
+  };
 
   return (
     <nav className={`nav ${show && "nav__black"}`}>
-      <img 
-        alt="Neflix logo" 
-        src= {PUBLIC_URL + '/assets/netflix_logo.png'}
+      <img
+        alt="Neflix logo"
+        src={PUBLIC_URL + "/assets/netflix_logo.png"}
         className="nav__logo"
-        onClick={()=>goIndex()}
+        onClick={() => goIndex()}
       />
       <div className="nav__right">
-      <input value={searchValue} onChange={handleChange} className="nav__input" type="text" placeholder="영화명, TV 프로그램명" />
-      <img
-        alt="User logged"
-        src={PUBLIC_URL + '/assets/netflix_profile_icon.png'}
-        className="nav__avatar"
-      />
+        <input
+          value={searchValue}
+          onChange={handleChange}
+          className="nav__input"
+          type="text"
+          placeholder="영화명, TV 프로그램명"
+        />
+        <img
+          alt="User logged"
+          src={PUBLIC_URL + "/assets/netflix_profile_icon.png"}
+          className="nav__avatar"
+        />
       </div>
     </nav>
-  )
+  );
 }
 
 export default React.memo(Nav);
